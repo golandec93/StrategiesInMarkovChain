5368e49e2956b73736a325b476ac8eb7f8e5821e
golandec93 <kasparf93@gmail.com>
2016-06-16 10:49:29 +0300

    complete 3-d algorithm on work

--- src/iteralg.py
+++ src/iteralg.py
@@ -1,69 +1,79 @@
 import numpy as np
-from src.utils import generate_states
-from src.utils import print_states
-from src.utils import get_sample
 
 
-def get_q(states):
-    q = {}
+def eval_behavior(states, debug_enabled=False):
+    def debug(*objects):
+        if debug_enabled is True:
+            res = ''
+            for obj in objects:
+                res += str(obj) + " "
+            print("[DEBUG] " + res)
+
+    def get_q(states):
+        q = {}
+        number_of_states = len(states)
+        for i in range(0, number_of_states):
+            probs, costs = states[i]
+            number_of_stratagies = len(probs)
+            qq = np.ndarray(number_of_stratagies)
+            for j in np.arange(0, number_of_stratagies):
+                qq[j] = (np.sum(probs[j] * costs[j]))
+            q[i] = qq
+        return q
+
+    # init data
+    q = get_q(states)
     number_of_states = len(states)
-    for i in range(0, number_of_states):
-        probs, costs = states[i]
-        number_of_stratagies = len(probs)
-        qq = np.ndarray(number_of_stratagies)
-        for j in np.arange(0, number_of_stratagies):
-            qq[j] = (np.sum(probs[j] * costs[j]))
-        q[i] = qq
-    return q
 
-
-# init data
-# number_of_states = 4
-# states = generate_states(number_of_states)
-states = get_sample()
-q = get_q(states)
-number_of_states = len(states)
-
-# initial step
-x = np.ndarray(number_of_states, int)
-for i in np.arange(0, number_of_states):
-    x[i] = (np.argmax(q[i]))
-
-stop_flag = 0
-iterations = 0
-result = {0: x}
-while stop_flag != 3 and iterations < 100:
-    # step 1 parameters: states, x
-    print(iterations)
-    p_matrix = np.ndarray((number_of_states, number_of_states))
-    r_matrix = np.ndarray((number_of_states, number_of_states))
-    q_cur = np.zeros(number_of_states)
+    # initial step
+    x = np.ndarray(number_of_states, int)
+    current_income = np.ndarray(number_of_states)
     for i in np.arange(0, number_of_states):
-        q_cur[i] = -q[i][x[i]]
-        probs, costs = states[i]
-        p_matrix[i] = probs[x[i]]
-        p_matrix[i][number_of_states - 1] = -1
-        p_matrix[i][i] -= 1
-    # print(p_matrix)
-    v = np.linalg.solve(p_matrix, q_cur)
-    g = v[number_of_states - 1]
-    v[number_of_states - 1] = 0
-    # print(v)
+        x[i] = (np.argmax(q[i]))
+        current_income[i] = max(q[i])
 
-    # step 2  //q_i_k + sum(p_i_j*v_j)
-    new_x = np.ndarray(number_of_states, int)
-    for i in range(0, number_of_states):
-        p, c = states[i]
-        number_of_strategies = len(p)
-        res = np.ndarray(number_of_strategies)
-        for k in range(0, number_of_strategies):
-            res[k] = q[i][k] + np.sum(v * p[k])
-        new_x[i] = res.argmax()
+    stop_flag = 0
+    iterations = 0
+    behavior = [x]
+    income = [current_income]
+    while stop_flag != 3 and iterations < 100:
+        # step 1 parameters: states, x
+        debug("iteration: ", iterations)
+        p_matrix = np.ndarray((number_of_states, number_of_states))
+        q_cur = np.zeros(number_of_states)
+        for i in np.arange(0, number_of_states):
+            q_cur[i] = -q[i][x[i]]
+            probs, costs = states[i]
+            p_matrix[i] = probs[x[i]]
+            p_matrix[i][number_of_states - 1] = -1
+            p_matrix[i][i] -= 1
+        debug("p_matrix:\n", p_matrix)
+        v = np.linalg.solve(p_matrix, q_cur)
+        g = v[number_of_states - 1]
+        v[number_of_states - 1] = 0
+        debug("v:\n", v)
+        debug('g:', g)
 
-    if np.array_equal(x, new_x):
-        stop_flag += 1
-    iterations += 1
-    result[iterations] = new_x
-    x = new_x
+        # step 2  q_i_k + sum(p_i_j*v_j)
+        new_x = np.ndarray(number_of_states, int)
+        for i in range(0, number_of_states):
+            debug("state: ", i)
+            p, c = states[i]
+            number_of_strategies = len(p)
+            res = np.ndarray(number_of_strategies)
+            for k in range(0, number_of_strategies):
+                res[k] = q[i][k] + np.sum(v * p[k])
+            debug("income for each strategy:\n", res)
+            current_income[i] = max(res)
+            new_x[i] = res.argmax()
 
-print(result)
+        if np.array_equal(x, new_x):
+            stop_flag += 1
+        iterations += 1
+        behavior.append(new_x + 1)
+        income.append(current_income)
+        x = new_x
+
+    debug("behavior", behavior)
+    debug("income", income)
+    return behavior, income
--- src/tests.py
+++ src/tests.py
@@ -1,8 +1,10 @@
 from src.utils import get_sample
 from src.utils import print_states
 import src.condecs as condecs
+import src.iteralg as iteralg
 
 states = get_sample()
 print_states(states)
-behavior, income = condecs.eval_behavior(states, debug_enabled=True)
-
+debug_enabled = True
+behavior1, income1 = condecs.eval_behavior(states, debug_enabled=debug_enabled)
+behavior2, income2 = iteralg.eval_behavior(states, debug_enabled=debug_enabled)
